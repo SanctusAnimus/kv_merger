fdb89ae98581461ea0100581a0ab996ac1f50101
SanctusAnimus <43399078+SanctusAnimus@users.noreply.github.com>
2022-04-21 01:21:22 +0300

    checks and formatting tweaks

--- merger.py
+++ merger.py
@@ -4,6 +4,10 @@ from pathlib import Path
 
 def merge_profile(profile: dict):
     cwd = Path.cwd()
+    if not profile["source_dir"]:
+        return
+    if not profile["target_file_name"]:
+        return
     src = cwd / profile["source_dir"]
     target = cwd / profile["target_file_name"]
     if not src.exists():
@@ -17,9 +21,15 @@ def merge_profile(profile: dict):
         for file_name in files:
             open_path = path.join(root, file_name)
             with open(open_path, 'r') as src:
-                content = src.read()
+                content = src.read().strip()
                 read_content.append(
-                    f"\n// START - {file_name} - START\n{content}\n// END - {file_name} - END\n"
+                    f"//--------------------------------------------------------------------\n"
+                    f"// START  - {file_name} - START\n"
+                    f"//--------------------------------------------------------------------\n"
+                    f"{content}\n"
+                    f"//--------------------------------------------------------------------\n"
+                    f"// END - {file_name} - END\n"
+                    f"//--------------------------------------------------------------------\n\n"
                 )
     read_content.append(f"\n{profile['postfix']}\n\n")
     with open(target, "w") as target_file:
